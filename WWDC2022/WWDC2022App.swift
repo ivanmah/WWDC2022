20d9370455535c2e9ff411f4872a1b6d81e0cc02
Ivan Mah <ivanmahjxdeveloper@gmail.com>
2022-06-14 11:23:15 +0800

    Initial Commit

---
+++ WWDC2022/WWDC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2022App.swift
+//  WWDC2022
+//
+//  Created by Ivan Mah on 14/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
